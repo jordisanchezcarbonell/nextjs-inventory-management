1cdbff16568763dfab715fffa961e92898013e43
Jordi Sánchez <jsanchez@eunoia.es>
2024-10-18 21:09:25 +0200

    Sidenavbar and redux

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    </main>
-  );
+  return <Dashboard />;
 }
